ef5e328d3f5cfbb8c0dfd659e61a1f33bcfc6ccc
Re20211101 <898626800@qq.com>
2022-06-10 22:23:28 +0800

    second hello git

--- src/main/java/cn/king/gitTset/Git_demo01.java
+++ src/main/java/cn/king/gitTset/Git_demo01.java
@@ -3,5 +3,6 @@ package cn.king.gitTset;
 public class Git_demo01 {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git2");
     }
 }
