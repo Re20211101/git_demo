b017d3992190116315b33095b1dab7be61afb1d8
Re20211101 <898626800@qq.com>
2022-06-11 10:57:18 +0800

    hot-fix hello git

--- src/main/java/cn/king/gitTset/Git_demo01.java
+++ src/main/java/cn/king/gitTset/Git_demo01.java
@@ -4,5 +4,6 @@ public class Git_demo01 {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git2");
+        System.out.println("hello Git3");
     }
 }
