7e5429337775a6aeeadd55ea4b49fc8651c7ee5c
Re20211101 <898626800@qq.com>
2022-06-11 11:08:36 +0800

    master hello git

--- src/main/java/cn/king/gitTset/Git_demo01.java
+++ src/main/java/cn/king/gitTset/Git_demo01.java
@@ -5,5 +5,6 @@ public class Git_demo01 {
         System.out.println("hello Git");
         System.out.println("hello Git2");
         System.out.println("hello Git3");
+        System.out.println("master git");
     }
 }
